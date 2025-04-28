49e94f7723046f0a793c8b6574126ebea25e23fc
Samar <samarasimha.reddy@innovatechs.com>
2025-04-28 12:50:38 +0530

    Scroll bottom navigation setup done

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {AppStackParamList} from './types';
-import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
-import {useTheme} from '../Utils/Globles';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import {useFont, useTheme} from '../Utils/Globles';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import IconF from 'react-native-vector-icons/Feather';
 
-//Screens
+//Main Screens
 import Drawer from '../Layout/Drawer';
 import Home from '../Screens/Main/Home';
 import Chats from '../Screens/Main/Chats';
@@ -15,14 +19,17 @@ import Post from '../Screens/Main/Post';
 import Search from '../Screens/Main/Search';
 import Settings from '../Screens/Main/Settings';
 
+//Stack Screens
 import Splash from '../Screens/Auth/Splash';
 import Login from '../Screens/Auth/Login';
 import Languages from '../Components/Languages';
 import Chat from '../Components/Chat';
+import {COLORS} from '../Utils/Colors';
+import {Text, View} from 'react-native';
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
 const DrawerStack = createDrawerNavigator<AppStackParamList>();
-const BottomStack = createBottomTabNavigator<AppStackParamList>();
+const BottomStack = createMaterialTopTabNavigator<AppStackParamList>();
 
 //Stack Navigation
 const Routes = () => {
@@ -64,22 +71,74 @@ const DrawerTabHand = () => {
 };
 
 //Bottom Navigation
-const BottomTabHandler = (props: any) => {
+const BottomTabHandler = () => {
   const {themeColors} = useTheme();
+  const {FONT_SIZE} = useFont();
+
+  const TabBarIcon = (iconName: string, focused: boolean) => {
+    return (
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: focused ? COLORS.MintGreen : 'transparent',
+          paddingHorizontal: hp('2.1%'),
+          paddingVertical: hp('0.5%'),
+          borderRadius: hp('1.5%'),
+        }}>
+        <IconF name={iconName} size={hp('2.3%')} color={COLORS.DarkBlack} />
+      </View>
+    );
+  };
+
+  const screens = [
+    {name: 'Home', component: Home, icon: 'home'},
+    {name: 'Chats', component: Chats, icon: 'message-circle'},
+    {name: 'Post', component: Post, icon: 'plus-square'},
+    {name: 'Search', component: Search, icon: 'search'},
+    {name: 'Settings', component: Settings, icon: 'settings'},
+  ];
 
   return (
     <BottomStack.Navigator
-      screenOptions={() => ({
-        headerShown: false,
-        tabBarActiveTintColor: themeColors.themeColor,
-        tabBarInactiveTintColor: '#959594',
-        flex: 1,
-      })}>
-      <BottomStack.Screen name={'Home'} component={Home} />
-      {/* <BottomStack.Screen name={'Chats'} component={Chats} />
-      <BottomStack.Screen name={'Post'} component={Post} />
-      <BottomStack.Screen name={'Search'} component={Search} /> */}
-      {/* <BottomStack.Screen name={'Settings'} component={Settings} /> */}
+      screenOptions={{
+        tabBarShowIcon: true,
+        tabBarLabelStyle: {
+          fontSize: FONT_SIZE.F_15,
+          fontWeight: 'bold',
+          color: COLORS.MintGreen,
+          textTransform: 'capitalize',
+        },
+        tabBarStyle: {
+          backgroundColor: themeColors.backGroundColor,
+        },
+        swipeEnabled: true,
+        tabBarIndicatorStyle: {backgroundColor: 'transparent'},
+      }}
+      tabBarPosition="bottom">
+      {screens.map(({name, component, icon}) => (
+        <BottomStack.Screen
+          key={name}
+          name={name}
+          component={component}
+          options={{
+            tabBarLabel: ({focused}) => (
+              <Text
+                style={{
+                  fontSize: FONT_SIZE.F_14,
+                  fontWeight: 'bold',
+                  color: COLORS.DarkBlack,
+                  textAlign: 'center',
+                  marginBottom: hp('1.5%'),
+                  marginTop: hp('0.5%'),
+                }}>
+                {name}
+              </Text>
+            ),
+            tabBarIcon: ({focused}) => TabBarIcon(icon, focused),
+          }}
+        />
+      ))}
     </BottomStack.Navigator>
   );
 };
